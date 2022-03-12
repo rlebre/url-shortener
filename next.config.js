73bfb5c7738be0a3e0101d5592a4223204fc2412
Rui Lebre <ruilebre@ua.pt>
2022-03-12 18:18:01 +0000

    Added dependencies; Added ignore json database; added global stypes; added redirect /l to /api/l

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/l/:shortUrl*',
+        destination: '/api/l/:shortUrl*',
+        permanent: true,
+      },
+    ]
+  },
 }
