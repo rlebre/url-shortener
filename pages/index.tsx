bb264df9f953c7ace143a02b8cd00aa130061cdc
Rui Lebre <ruilebre@ua.pt>
2022-03-14 12:30:28 +0000

    Added custom components to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,36 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useState } from 'react';
 import CreateForm from '../components/create-form/create-form';
 import Footer from '../components/footer/footer';
+import { Modal } from '../components/modal/modal';
 
 const Home: NextPage = () => {
+  const [showModal, setShowModal] = useState(true);
+
+  const cenas = () => setShowModal(false);
+
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2 text-sm'>
-      <Head>
-        <title>Link Shortener</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+    <>
+      {showModal && <Modal handleCloseClick={() => cenas()}>ola</Modal>}
 
-      <div className='mb-20 flex w-full flex-col items-center justify-center'>
-        <h1 className='mt-3 text-2xl font-bold'>Link shortener</h1>
+      <div className='flex min-h-screen flex-col items-center justify-center py-2 text-sm'>
+        <Head>
+          <title>Link Shortener</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+
+        <div className='mb-20 flex w-full flex-col items-center justify-center'>
+          <h1 className='mt-3 text-2xl font-bold'>Link shortener</h1>
+        </div>
+
+        <div className='flex w-full flex-col items-center md:w-2/3'>
+          <CreateForm />
+        </div>
+
+        <Footer />
       </div>
-
-      <div className='flex w-full flex-col items-center md:w-2/3'>
-        <CreateForm />
-      </div>
-
-      <Footer />
-    </div>
+    </>
   );
 };
 
