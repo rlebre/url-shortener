05d01acdbc3b98502fa7adb3900f425499a174e0
Rui Lebre <ruilebre@ua.pt>
2022-03-18 16:33:13 +0000

    Fixed return key on create form; Fixed short url when not set

--- pages/api/l/create.ts
+++ pages/api/l/create.ts
@@ -41,7 +41,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       emailSender
         .sendConfirmationEmail({
           confirmationHash,
-          shortUrl,
+          shortUrl: short,
           fullUrl,
           toEmail: email,
         })
